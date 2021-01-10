cb070f1264f588d4b43f29859134e3bd7afbe69e
paramsrikant <32132401+paramsrikant@users.noreply.github.com>
2021-01-10 09:49:18 +0530

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello World again from GitHub - Poll SCM ");
+		System.out.println("Hello World again from GitHub - Poll SCM");
 	}
 }
